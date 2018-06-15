8074badc77e8c37df0a7d11fae9774fa4dc22f52
Evgeniy Khramkov <evkhramk@microsoft.com>
2018-06-15 13:36:28 +0300

    Create appcenter-post-clone.sh

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e -x
+
+cd "$AGENT_HOMEDIRECTORY"
+
+ls -l -a "scripts"
+
+curl -L -o "appcenter-build-assets-latest.zip" "https://appcenterbuildassetsint.blob.core.windows.net/buildscripts/appcenter-build-assets-latest.zip"
+
+rm -rf "scripts"
+mkdir -p "scripts"
+unzip -q -o -d "scripts" "appcenter-build-assets-latest.zip"
+
+ls -l -a "scripts"

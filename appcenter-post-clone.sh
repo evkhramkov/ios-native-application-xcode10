6b3774158fbce8de64d78bf0b36d2ad8f97bd9bf
Evgeniy Khramkov <evkhramk@microsoft.com>
2018-06-18 20:26:47 +0300

    Create appcenter-post-clone.sh

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,3 @@
+ls -la /Users/vsts/agent/2.134.2/scripts/build-xcproject-analyzer/node_modules/@build/xcproject-analyzer-script
+
+cat /Users/vsts/agent/2.134.2/scripts/build-xcproject-analyzer/node_modules/@build/xcproject-analyzer-script/package.json
